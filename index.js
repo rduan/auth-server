106b073781aac36166b776df0b460bdd99a8d082
RD <dxloader@gmail.com>
2018-02-11 13:48:07 +1100

    add packages

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const app = express(
 
 );
 //App setup
-
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
 
 //Server setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
-console.log('svr listening on:' + port);
+console.log('svr listening on: ', port);
